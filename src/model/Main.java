3fd170255aa485dabd91d6375e372b9fcf9c6a47
naveenvpai <pai.naveen357@gmail.com>
2016-05-28 08:28:33 -0700

    Test array/matrix printing

--- src/model/Main.java
+++ src/model/Main.java
@@ -6,12 +6,28 @@ public class Main {
         for (int i = 0; i <= 100; i++) {
             if (Math.sqrt((double)i)%1==0) System.out.println(i);
         }
+        print(m(a(1,2,3,4),a(5,6,7,8)));
     }
 
-    private int[] a(int... els) {
+    private static int[] a(int... els) {
         return els;
     }
-    private int[][] m(int[]... rows) {
+
+    private static int[][] m(int[]... rows) {
         return rows;
+    }
+
+    private static void print(int... els) {
+        for (int i = 0; i < els.length; i++) {
+            String spacer = i > 0 ? " " : "";
+            System.out.print(spacer+els[i]);
+        }
+        System.out.println();
+    }
+
+    private static void print(int[]... rows) {
+        for (int[] row : rows) {
+            print(row);
+        }
     }
 }
